190cef63be0905164e87c29196d7b15b27bdc440
chensoul <zhijun.chen@dmall.com>
2023-01-18 12:09:32 +0800

    update workflow

---
+++ build_about.py
@@ -0,0 +1,31 @@
+import httpx
+import pathlib
+import re
+import sys
+
+root = pathlib.Path(__file__).parent.resolve()
+
+def replace_chunk(content, marker, chunk, inline=False):
+    r = re.compile(
+        r"<!\-\- {} starts \-\->.*<!\-\- {} ends \-\->".format(marker, marker),
+        re.DOTALL,
+    )
+    if not inline:
+        chunk = "\n{}\n".format(chunk)
+    chunk = "<!-- {} starts -->{}<!-- {} ends -->".format(marker, chunk, marker)
+    return r.sub(chunk, content)
+
+def fetch_github_readme():
+     return httpx.get(
+         "https://raw.githubusercontent.com/chensoul/chensoul/main/README.md"
+     )
+
+if __name__ == "__main__":
+    inputfile = sys.argv[1]
+    file = root.joinpath(inputfile)
+
+    readme = fetch_github_readme()
+    readme_contents = file.open().read()
+    rewritten = replace_chunk(readme_contents, "readme", md)
+
+    readme.open("w").write(rewritten)
